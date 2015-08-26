85c8e13a6b337ddd560fd66fd70c317b04539244
aaronjfinley <aaronjfinley@gmail.com>
2015-08-26 17:55:38 -0500

    fixed validations to check duplicate train_id on date

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,23 +1,34 @@
 class Arrival < ActiveRecord::Base
 
-  validates :train_id, :presence => true
   validate :unique_date_train_id
+
+  validates :train_id, :presence => true
   validates :platform, :presence => true
 
   belongs_to :train , :class_name => "Train", :foreign_key => "train_id"
 
   def unique_date_train_id
 
+    #if new entry, must be unique train_id for today's date
+    #if editing entry, must be unique train_id for created_at date
+
     dates_for_train_id = []
     Arrival.all.each do |arr|
-      #find all created_at dates for train_id excluding the one you are editing
-      if arr.train_id == train_id && arr.id != id
+      #find all created_at dates for train_id
+      if arr.train_id == train_id && arr[:id] != id
         dates_for_train_id.push(arr.created_at.in_time_zone("Central Time (US & Canada)").to_date)
       end
     end
 
-    if dates_for_train_id.include? Date.today
-      errors.add(:platform,'already added for that train today')
+    if Arrival.all.any? { |h| h.id == id }
+      arr = Arrival.find(id)
+      if dates_for_train_id.include? arr.created_at.in_time_zone("Central Time (US & Canada)").to_date
+        errors.add(:platform,'already added for train')
+      end
+    else
+      if dates_for_train_id.include? Date.today
+        errors.add(:platform,'already added for train')
+      end
     end
   end
 
